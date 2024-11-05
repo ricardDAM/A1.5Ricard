21a6174eeb67b6fc28f50f5dabf136a6afcceebb
Ricard Ribas <ririhe@inspalamos.cat>
2024-11-05 16:43:35 +0100

    ex3Acabat

--- repetitivaricard3/Program.cs
+++ repetitivaricard3/Program.cs
@@ -6,24 +6,28 @@
         {
             //3.Fes un programa per a saber quants valors positius i negatius hem introduït per teclat
 
+            //variables
             int numero;
             int contPositius = 0;
             int contNegatius = 0;
 
+            //entrada
             Console.Write("Introdueix un numero positiu o negatiu: ");
             numero = Convert.ToInt32(Console.ReadLine());
 
+            //algorisme
             while (numero != 0)
             {
 
                 if (numero > 0) contPositius++;
-                else if (numero < 0) contNegatius++;
+                else  contNegatius++;
 
                 Console.Write("Introdueix un altre numero positiu o negatiu: ");
                 numero = Convert.ToInt32(Console.ReadLine());
 
                 
             }
+            //sortida
             Console.WriteLine($"Has introduit {contPositius} numeros positius i {contNegatius} numeros negatius.");
 
         }
