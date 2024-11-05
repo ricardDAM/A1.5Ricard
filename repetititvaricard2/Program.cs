af74c0e0397ee409aa2bc343a1ac909bbcf62d1f
Ricard Ribas <ririhe@inspalamos.cat>
2024-11-05 16:38:46 +0100

    ex1AcabatFinal

--- repetititvaricard2/Program.cs
+++ repetititvaricard2/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace repetititvaricard2
+﻿
+using System.IO;
+
+namespace repetititvaricard2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //2.Fes un programa per contar quants valors enters introdueix un usuari per Consola.
+            //Deixarem de contar valors en trobar el valor 0.Fes el mateix programa però agafant les dades del fitxer NUMEROS.TXT.FI = null.
+
+            //variables
+            string linia;
+            int numero;
+
+            //entrada
+            StreamReader sr = new StreamReader("NUMEROS.TXT");
+
+            //algorisme i sortida
+            linia = sr.ReadLine();
+           
+            
+            while (linia != null)
+            {
+
+               numero =Convert.ToInt32(linia);
+                Console.WriteLine(numero);
+                linia= sr.ReadLine();
+             
+            }
+
+
+
+
+            sr.Close();
+
+
+
+
         }
     }
 }
