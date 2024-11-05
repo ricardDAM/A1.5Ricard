477c46a86daf9e4b912a3587b00e7650da2e7007
Ricard Ribas <ririhe@inspalamos.cat>
2024-11-05 16:43:40 +0100

    ex4Acabat

--- repetitivaricard4/Program.cs
+++ repetitivaricard4/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace repetitivaricard4
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace repetitivaricard4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //4.Fes un programa per a saber quants valors positius i negatius hem introduït per teclat
+            //Fes el mateix programa amb FI = null i dades procedents de fitxer NUMEROS.TXT
+            
+            //variables
+            string linia;
+            int numero;
+            int contPositius = 0;
+            int contNegatius = 0;
+
+            //entrada
+            StreamReader sr = new StreamReader("NUMEROS.TXT");
+
+            //algorisme 
+            linia = sr.ReadLine();
+ 
+            while (linia != null )
+            {
+                numero = Convert.ToInt32(linia);
+                if (numero > 0) contPositius++;
+                else  contNegatius++;
+               
+                linia = sr.ReadLine();
+            }
+            sr.Close();
+            // sortida
+            Console.WriteLine($"S'ha trobat {contPositius} numeros positius i {contNegatius} numeros negatius.");
         }
     }
 }
