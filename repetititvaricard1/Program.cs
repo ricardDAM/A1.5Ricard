15c029fc6155719f81fc2e495596d71ddd5aef2c
Ricard Ribas <ririhe@inspalamos.cat>
2024-11-05 16:38:39 +0100

    ex1AcabatFinal

--- repetititvaricard1/Program.cs
+++ repetititvaricard1/Program.cs
@@ -9,12 +9,15 @@ namespace repetititvaricard1
 
             //1. Fes un programa per contar quants valors enters introdueix un usuari per Consola.
             //Deixarem de contar valors en trobar el valor 0.
-          
+            
+            //variables
             int cont = 0, num;
 
+            //entrada
             Console.WriteLine("Diguem el valor de num: ");
             num= Convert.ToInt32(Console.ReadLine());
 
+           //algorisme i sortida
             while (num!=0)
             {
                 cont++;
@@ -22,7 +25,7 @@ namespace repetititvaricard1
                 num = Convert.ToInt32(Console.ReadLine());
 
             }
-            Console.WriteLine($"Has escrit {num} ");
+            Console.WriteLine($"Has escrit {cont} numeros");
 
         }
     }
