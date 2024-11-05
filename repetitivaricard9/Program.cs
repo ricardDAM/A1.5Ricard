ca83c2883fa38de1e4bb3183209e4043b82b5ee8
Ricard Ribas <ririhe@inspalamos.cat>
2024-11-05 16:44:36 +0100

    ex9Acabat

--- repetitivaricard9/Program.cs
+++ repetitivaricard9/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace repetitivaricard9
+﻿using System.Diagnostics.Metrics;
+
+namespace repetitivaricard9
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //9.Fer un programa que donat un número enter positiu et digui quantes xifres té.
+            
+            //variables
+            int numero;
+            int cont=0;
+            int aux;
+
+            //entrada
+
+            Console.Write("Diguem un valor per numero: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            aux = numero;
+
+            //algorisme i sortida
+
+            while (aux != 0)
+            {
+                cont++;
+                aux = aux / 10;
+            }
+
+            Console.WriteLine($"El numero te {cont} digits");
+
         }
     }
 }
