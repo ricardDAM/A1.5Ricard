7fef983101c7d06cfeed364e6ecda1153d8521e4
Ricard Ribas <ririhe@inspalamos.cat>
2024-11-05 16:44:42 +0100

    ex10Acabat

--- repetitivaricard9.10/Program.cs
+++ repetitivaricard9.10/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.Intrinsics.X86;
-using System.Threading;
-
+﻿
 namespace repetitivaricard9._10
 {
     internal class Program
@@ -11,8 +9,30 @@ namespace repetitivaricard9._10
             de vegades que surti 1 i 6 sigui el mateix.Conta quantes vegades hem llançat el dau i quantes
             vegades ha aparegut el valor 6*/
 
+             //variables
+                int numeroDau;
+                int cont1=0;
+                int cont6=0;
+                int contGeneral=0;
 
+            //entrada
+            Random rand = new Random();
+            numeroDau = rand.Next(1, 7);
 
+            //algorisme
+          // la primera condicio es que cont sigui diferent a zero
+            while (cont1 != cont6 || cont1==0 || cont6==0 ){
+
+                if (numeroDau == 1) cont1++;
+                else if (numeroDau == 6) cont6++;
+              
+                contGeneral++;
+                numeroDau = rand.Next(1, 7);
+
+            }
+            //sortida
+            Console.WriteLine($"Hem llençat el dau {contGeneral} vegades");
+            Console.WriteLine($"Ha caigut al 6 {cont6} vegades");
         }
     }
 }
